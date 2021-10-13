a5d43c323312164c3ead621202da78d0b76cc1bc
hnor <hamid.norouzi@yahoo.com>
2021-10-14 01:42:51 +0330

    try to fix xml deserialization error on some cammera

---
+++ Classes/OnvifObjects/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SgiOnvifRestApiGW.Classes.OnvifObjects
+{
+    public class Class
+    {
+    }
+}
